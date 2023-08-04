a7f7ca3cfa9138ca118aac2ea30002e2d88938cc
impactog <48831675+impactog@users.noreply.github.com>
2023-08-04 19:40:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 
+//  Referencia: https://www.rudderstack.com/docs/event-spec/standard-events/page/
+
 document.querySelector("#web-visit-form").addEventListener("submit", function(event){
-    event.preventDefault()
-    
-    //  Referencia: https://www.rudderstack.com/docs/event-spec/standard-events/page/
-    rudderanalytics.page({ url: document.querySelector("#email").value })
-// rudderanalytics.getUserId(), 
+    event.preventDefault();
+    rudderanalytics.page({ url: document.querySelector("#email").value });
 } )
 
 
 document.querySelector("#email-form").addEventListener("submit", function(event){
-    event.preventDefault()
+    event.preventDefault();
+    const email = document.querySelector("#email").value;
+    rudderanalytics.identify(email, { email: email });
+} )
 
-    const emailForm = document.querySelector("#email")
-    const email = emailForm.value
+document.querySelector("#linkedin-form").addEventListener("submit", function(event){
+    event.preventDefault();
+    rudderanalytics.identify(rudderanalytics.getUserId(), {website: document.querySelector("#url_linkedin").value });
+} )
 
-    rudderanalytics.identify(email, { email: email })
+document.querySelector("#foto-form").addEventListener("submit", function(event){
+    event.preventDefault();
+    rudderanalytics.identify(rudderanalytics.getUserId(), {avatar: document.querySelector("#url_foto").value });
+} )
 
-    emailForm.value = ""
+document.querySelector("#evento-form").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    if (typeof rudderanalytics.getUserTraits().eventos !== 'undefined') {
+        var eventos = rudderanalytics.getUserTraits().eventos;
+    } else {
+        var eventos = []; 
+    }
+
+    eventos.push ({"date": document.querySelector("#evento_fecha").value, "title": document.querySelector("#evento").value, "country": document.querySelector("#pais").value, "type": document.querySelector("#evento_tipo").value});    
+    rudderanalytics.identify(rudderanalytics.getUserId(), {"eventos": eventos})
+    
 } )
